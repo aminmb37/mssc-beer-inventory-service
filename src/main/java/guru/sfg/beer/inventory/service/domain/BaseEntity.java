8e21afe8d32bcb91ef60d90ff6db0bfad1785c58
aminmb37 <amin.mb37@gmail.com>
2020-11-02 23:36:38 +0100

    Configuring MySQL.

--- src/main/java/guru/sfg/beer/inventory/service/domain/BaseEntity.java
+++ src/main/java/guru/sfg/beer/inventory/service/domain/BaseEntity.java
@@ -36,23 +36,23 @@ import java.util.UUID;
 @Getter
 @NoArgsConstructor
 @MappedSuperclass
-public class BaseEntity {
+public class BaseEntity
+{
 
-    public BaseEntity(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate) {
+    public BaseEntity(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate)
+    {
         this.id = id;
         this.version = version;
         this.createdDate = createdDate;
         this.lastModifiedDate = lastModifiedDate;
     }
 
+
     @Id
     @GeneratedValue(generator = "UUID")
-    @GenericGenerator(
-            name = "UUID",
-            strategy = "org.hibernate.id.UUIDGenerator"
-    )
-    @Type(type="org.hibernate.type.UUIDCharType")
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false )
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Type(type = "org.hibernate.type.UUIDCharType")
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
 
     @Version
@@ -65,7 +65,9 @@ public class BaseEntity {
     @UpdateTimestamp
     private Timestamp lastModifiedDate;
 
-    public boolean isNew() {
+
+    public boolean isNew()
+    {
         return this.id == null;
     }
 }
--- src/main/java/guru/sfg/beer/inventory/service/domain/BeerInventory.java
+++ src/main/java/guru/sfg/beer/inventory/service/domain/BeerInventory.java
@@ -16,6 +16,8 @@
  */
 package guru.sfg.beer.inventory.service.domain;
 
+import javax.persistence.Column;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,25 +27,30 @@ import javax.persistence.Entity;
 import java.sql.Timestamp;
 import java.util.UUID;
 
+import org.hibernate.annotations.Type;
+
 /**
  * Created by jt on 2019-01-26.
  */
+
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
-public class BeerInventory extends BaseEntity{
-
+public class BeerInventory extends BaseEntity {
     @Builder
-    public BeerInventory(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate, UUID beerId,
-                         String upc, Integer quantityOnHand) {
+    public BeerInventory(UUID id, Long version, Timestamp createdDate,
+                         Timestamp lastModifiedDate, UUID beerId, String upc, Integer quantityOnHand) {
         super(id, version, createdDate, lastModifiedDate);
         this.beerId = beerId;
         this.upc = upc;
         this.quantityOnHand = quantityOnHand;
     }
 
+    @Type(type = "org.hibernate.type.UUIDCharType")
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID beerId;
+
     private String upc;
     private Integer quantityOnHand = 0;
 }
